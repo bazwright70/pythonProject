ff45ea31a36eae8bd42dad3df479f56e086f63d6
Bruian Wright <bazwright1970@gmail.com>
2024-09-04 11:59:58 +0100

    latest commit

--- sl_script.py
+++ sl_script.py
@@ -3,9 +3,9 @@ import urllib.request
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
-ctx.verify_mode =ssl.CERT_NONE
+ctx.verify_mode = ssl.CERT_NONE
 
-ip = "8.8.8.8"
+ip = input("What ip address do you want to lookup:")
 
 url = f"https://ipapi.co/{ip}/json"
 contents = urllib.request.urlopen(url, context=ctx).read()
